889247026fd2e594aa932646b564faf1c7b3ee5e
smarble <hellosandramarble@gmail.com>
2018-08-08 15:09:56 -0500

    setting up github hosting
    adding portfolio package to github hosting. first attempt

---
+++ portfolio/js/script.js
@@ -0,0 +1,81 @@
+// Portfolio
+// Usefull console commands: console.log("Click")
+
+
+// wrap all in document ready function
+$(document).ready(function(){
+  console.log("JavaScript loaded, Huzzah!")
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+//
+// From Emily
+// var red = document.getElementById("red");
+// var orange = document.getElementById("orange");
+// var yellow = document.getElementById("yellow");
+// var green = document.getElementById("green");
+// var teal = document.getElementById("teal");
+// var blue = document.getElementById("blue");
+// var indigo = document.getElementById("indigo");
+// var violet = document.getElementById("violet");
+//
+// red.onclick = function () {
+//   if(red.className = "start") {
+//     console.log("growing red");
+//     red.className = ("end");
+//   }
+// }
+// orange.onclick = function () {
+//   if(orange.className = "start") {
+//     console.log("growing orange");
+//     orange.className = ("end");
+//   }
+// }
+// yellow.onclick = function () {
+//   if(yellow.className = "start") {
+//     console.log("growing yellow");
+//     yellow.className = ("end");
+//   }
+// }
+// green.onclick = function () {
+//   if(green.className = "start") {
+//     console.log("growing green");
+//     green.className = ("end");
+//   }
+// }
+// teal.onclick = function () {
+//   if(teal.className = "start") {
+//     console.log("growing teal");
+//     teal.className = ("end");
+//   }
+// }
+// blue.onclick = function () {
+//   if(blue.className = "start") {
+//     console.log("growing blue");
+//     blue.className = ("end");
+//   }
+// }
+// indigo.onclick = function () {
+//   if(indigo.className = "start") {
+//     console.log("growing indigo");
+//     indigo.className = ("end");
+//   }
+// }
+// violet.onclick = function () {
+//   if(violet.className = "start") {
+//     console.log("growing violet");
+//     violet.className = ("end");
+//   }
+// }
+// // could this have been done with less repetitive coding if I had used an array?
