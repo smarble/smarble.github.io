fb362bd7bd84cf52560b8e762b8a319d2a663cab
smarble <hellosandramarble@gmail.com>
2018-08-10 13:11:13 -0500

    Save added states picture to redo states game

--- statesGame/js/script.js
+++ statesGame/js/script.js
@@ -1,4 +1,5 @@
-// wrap all in document ready function
+// STATES GAME REDO
+// WRAP EVERYTHING IN DOCUMENT READY FUNCTION
 $(document).ready(function(){
   console.log("javascript loaded successfully")
   $(".revealedCapital").hide();
@@ -9,55 +10,37 @@ $(document).ready(function(){
     $(".stateNameCalifornia").fadeOut(0);
     // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealSacramento").fadeIn(0);
-  });
+    });
 
   $(".alaska").click(function() {
     console.log("click")
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".stateNameAlaska").fadeOut(0);
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealJuneau").fadeIn(0);
   });
 
   $(".colorado").click(function() {
     console.log("click")
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".stateNameColorado").fadeOut(0);
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealDenver").fadeIn(0);
   });
 
   $(".arizona").click(function() {
     console.log("click")
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".stateNameArizona").fadeOut(0);
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealPhoenix").fadeIn(0);
   });
 
   $(".arkansas").click(function() {
     console.log("click")
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".stateNameArkansas").fadeOut(0);
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealLittleRock").fadeIn(0);
   });
 
   $(".alabama").click(function() {
     console.log("click")
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".stateNameAlabama").fadeOut(0);
-    // you can pass in a milisecond duration for fadeIn or a keyword like slow, normal or fast
     $(".revealMontgomery").fadeIn(0);
   });
-
-
-
-
-
-
-
-
 
 // end of document ready  function
 });
